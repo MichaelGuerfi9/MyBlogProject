213ece3fa24e3a5b525036b9003d493eeeecacf5
Michael Guerfi <michaelguerfi@gmail.com>
2016-02-08 15:46:16 +0100

    Suppression bugs

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
 require_once("model/connexion.php");
 global $pdo;
 session_start();
@@ -7,31 +6,14 @@ if(!isset($_SESSION['user'])){
     header("Location: /login.php");
     exit;
 }
-=======
-  require_once("model/connexion.php");
-  global $pdo;
-  session_start();
-  if(!isset($_SESSION['user'])){
-    header("Location: /login.php");
-    exit;
-  }
->>>>>>> origin/master
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<<<<<<< HEAD
     <meta charset="UTF-8">
     <title>PokéManiac</title>
 </head>
 <body>
 <h1>Bonjour <?php echo $_SESSION['user']; ?></h1>
-=======
-  <meta charset="UTF-8">
-  <title>PokéManiac</title>
-</head>
-<body>
-  <h1>Bonjour <?php echo $_SESSION['user']; ?></h1>
->>>>>>> origin/master
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 <?php
 require_once("model/connexion.php");
 session_start();
 if(isset($_POST['logout'])){
     unset($_SESSION['user']);
-=======
-<?php 
-require_once("model/connexion.php");
-session_start();
-if(isset($_POST['logout'])){
-  unset($_SESSION['user']);
->>>>>>> origin/master
 }
 if(isset($_SESSION['user'])){
-    header("Location: /index.php");
+    header("Location: ../index.php");
     exit;
 }
 $errormessage = null;
 if(isset($_POST['username'])){
-<<<<<<< HEAD
     global $pdo;
     $stmt = $pdo->prepare("SELECT * FROM users
                          WHERE login = :login");
@@ -33,35 +24,15 @@ if(isset($_POST['username'])){
         $errormessage = "Wrong password";
     }else{
         $_SESSION['user'] = $result["login"];
-        header("Location: /index.php");
+        header("Location: ../index.php");
         exit;
     }
-=======
-  global $pdo;
-  $stmt = $pdo->prepare("SELECT * FROM users 
-                         WHERE login = :login");
-  $stmt->bindParam("login",$_POST['username']);
-  $stmt->execute();
-  $result = $stmt->fetch();
-  if($result === false){
-    $errormessage = "Wrong username";
-  }elseif (empty($_POST["password"])) {
-    $errormessage = "No password";
-  }elseif (sha1($_POST["password"]) != $result["password"]) {
-    $errormessage = "Wrong password";
-  }else{
-    $_SESSION['user'] = $result["login"];
-    header("Location: /index.php");
-    exit;
-  }
->>>>>>> origin/master
 }
 ?>
 
 <!DOCTYPE html>
 <html lang="fr">
 <head>
-<<<<<<< HEAD
     <meta charset="UTF-8">
     <title>PokéManiac</title>
 </head>
@@ -73,18 +44,5 @@ Merci de vous connecter :
     <input type="submit" value="Se connecter">
 </form>
 <?php echo $errormessage;?>
-=======
-  <meta charset="UTF-8">
-  <title>PokéManiac</title>
-</head>
-<body>
-  Merci de vous connecter :
-  <form method="POST">
-  Login : <input type="text" name="username"><br>
-  Password : <input type="text" name="password"><br>
-  <input type="submit" value="Se connecter">
-  </form>
-  <?php echo $errormessage;?>
->>>>>>> origin/master
 </body>
 </html>
