22ee875b0ad545ef54a9552fc0fece467d42853a
Michael Guerfi <michaelguerfi@gmail.com>
2016-02-08 14:57:16 +0100

    Ajout .gitignore

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+require_once("model/connexion.php");
+global $pdo;
+session_start();
+if(!isset($_SESSION['user'])){
+    header("Location: /login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PokéManiac</title>
+</head>
+<body>
+<h1>Bonjour <?php echo $_SESSION['user']; ?></h1>
+</body>
+</html>
